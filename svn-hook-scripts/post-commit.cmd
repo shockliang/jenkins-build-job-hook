b67a6498c63496e64d5346fedc10381fd72c26b4
Shock <shockwater@gmail.com>
2017-06-30 17:53:05 +0800

    Add variables to store paths

--- svn-hook-scripts/post-commit.cmd
+++ svn-hook-scripts/post-commit.cmd
@@ -1,15 +1,21 @@
 :: Your directory path.
-cd C:\node-scripts
-
+set triggerPath=C:\node-scripts
+:: The app folder 
+set appPath=%triggerPath%\trigger
+:: Your project name. It will used to create folder.
+set projName=SSC
 set repo=%1
 set rev=%2
 
-:: Your project name. It will used to create folder.
-set projName=SSC
+cd %triggerPath%
+
+if not exist %triggerPath%\%projName% mkdir %projName%
 
 :: Export the revision commit path to text file after commit success.
 svnlook changed -r %rev% %repo%>%projName%\%rev%.txt
 
+cd %appPath%
+
 path C:\Program Files\nodejs
 
-node trigger.js
+node app.js build -p %triggerPath%\%projName%\%rev%.txt
