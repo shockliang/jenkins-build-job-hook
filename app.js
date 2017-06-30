a8f379391a95aa79d9de5835a8a5efd2a9485b1f
Shock <shockwater@gmail.com>
2017-06-30 17:04:36 +0800

    Using axios to request jenkins build the job

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const yargs = require('yargs');
 const axios = require('axios');
+const fs = require('fs');
 const argsOptions = require('./args.options.js');
+const {Jobs} = require('./utils/jobs');
 
 const argv = yargs
     .command('build', 'build the remote jenkins job', {
@@ -10,11 +12,17 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-var command = argv._[0];
+if(fs.existsSync(argv.path)) {
+    fs.readFile(argv.path, (err, data) => {
+        if(err) throw err;
+        var paths = data.toString().split(/\r?\n/);
+        var jobs = new Jobs();
+        var job = jobs.findJobByPath(data.toString());
 
-// axios.get(apiUrl).then((response) => {
-//     console.log('Has triggered!');
-// }).catch((e) => {
-//     console.log(e);
-// });
-
+        axios.get(jobs.getUri(job)).then((response) => {
+            console.log('send request successfully');
+        }).catch((e) => {
+            console.log(e);
+        });
+    });
+}
